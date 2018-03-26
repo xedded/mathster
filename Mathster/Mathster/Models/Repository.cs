8b6d20429ad0afd62597aaafb7ff9a7fbae4eab2
pontus.segerstad@gmail.com <pontus.segerstad@gmail.com>
2018-03-26 16:17:12 +0200

    Layoutpage

--- Mathster/Mathster/Models/Repository.cs
+++ Mathster/Mathster/Models/Repository.cs
@@ -11,12 +11,38 @@ namespace Mathster.Models
         public MultiplikationIndexVM MultiplicationRandomizer()
         {
             Random rdm = new Random();
-            int tal1 = rdm.Next(0, 11);
-            int tal2 = rdm.Next(0, 11);
+            int number1 = rdm.Next(0, 11);
+            int number2 = rdm.Next(0, 11);
+
+            int product = number1 * number2;
+            int[] arrayProduct = new int[2] { number1, number2};
+
+            int rangeMin = product - 5;
+            if (rangeMin<0)
+            {
+                rangeMin = 0;
+            }
+            int rangeMax = product + 5;
+
+            int fakeNumber1 = rdm.Next(rangeMin, rangeMax);
+            int fakeNumber2 = rdm.Next(rangeMin, rangeMax);
+            int fakeNumber3 = rdm.Next(rangeMin, rangeMax);
+            int[] arrayFakeNumbers = new int[4] { product, fakeNumber1, fakeNumber2, fakeNumber3};
 
 
 
-            return
+            MultiplikationIndexVM multiplikationIndexVM = new MultiplikationIndexVM
+            {
+                CorrectAnswer = product,
+                MultipliedFactors = arrayProduct,
+                ResultOptions = arrayFakeNumbers
+
+
+            };
+
+
+
+            return multiplikationIndexVM;
         }
 
 
