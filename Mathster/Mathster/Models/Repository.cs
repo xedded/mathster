e4306cf6f3e7f29acb596fe0a91eadfd234b447b
Pardis <pardisghadrdan@gmail.com>
2018-03-27 10:25:16 +0200

    Addition added

--- Mathster/Mathster/Models/Repository.cs
+++ Mathster/Mathster/Models/Repository.cs
@@ -58,5 +58,58 @@ namespace Mathster.Models
             };
             return multiplikationIndexVM;
         }
+
+        //Addition
+        public AdditionIndexVM AdditionRandomizer()
+        {
+            Random rdm = new Random();
+            int number1 = rdm.Next(1, 86);  
+            int number2 = rdm.Next(5, 16);
+
+            int sum = number1 + number2;
+            int[] arraySum = new int[2] { number1, number2 };
+
+            int rangeMin = sum - 10;
+            if (rangeMin < 0)
+            {
+                rangeMin = 0;
+            }
+            int rangeMax = sum + 10;
+
+
+            int fakeNumber1 = rdm.Next(rangeMin, rangeMax);
+            while (fakeNumber1 == sum)
+            {
+                fakeNumber1 = rdm.Next(rangeMin, rangeMax);
+            }
+            int fakeNumber2 = rdm.Next(rangeMin, rangeMax);
+            while (fakeNumber2 == fakeNumber1 || fakeNumber2 == sum)
+            {
+                fakeNumber2 = rdm.Next(rangeMin, rangeMax);
+            }
+            int fakeNumber3 = rdm.Next(rangeMin, rangeMax);
+            while (fakeNumber3 == fakeNumber2 || fakeNumber3 == fakeNumber1 || fakeNumber3 == sum)
+            {
+                fakeNumber3 = rdm.Next(rangeMin, rangeMax);
+            }
+
+            List<int> list = new List<int> { sum, fakeNumber1, fakeNumber2, fakeNumber3 };
+
+            // Ändringar!!
+
+            List<int> sortedList = list.OrderBy(v => v).ToList();
+            int[] arrayFakeNumbers = sortedList.ToArray();
+            //int[] arrayFakeNumbers = new int[4] { product, fakeNumber1, fakeNumber2, fakeNumber3};
+
+            AdditionIndexVM additionIndexVM = new AdditionIndexVM
+            {
+                AddedNumbers = arraySum,
+                ResultOptions=arrayFakeNumbers,
+                CorrectAnswer= sum,
+
+            };
+            return additionIndexVM;
+
+        }
     }
 }
