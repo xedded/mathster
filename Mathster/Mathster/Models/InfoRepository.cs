d491c8819fa179f90ef3bd87139d8aafd024094a
Pardis <pardisghadrdan@gmail.com>
2018-04-05 13:27:10 +0200

    added css page for infopage and content in Info index file

--- Mathster/Mathster/Models/InfoRepository.cs
+++ Mathster/Mathster/Models/InfoRepository.cs
@@ -14,19 +14,19 @@ namespace Mathster.Models
             switch (gameType)
             {
                 case GameType.Multiplication:
-                    model.Info = "https://www.youtube.com/embed/YXFWS4cmcsA?rel=0&autoplay=1";
+                    model.Info = @"C:/Users/Administrator/source/repos/MathsterAcademy/Mathster/Mathster/wwwroot/subt-1.png";
                     return model;
 
                 case GameType.Division:
-                    model.Info = "https://www.youtube.com/embed/4S-69OQokFA?rel=0&autoplay=1";
+                    model.Info = "";
                     return model;
 
                 case GameType.Addition:
-                    model.Info = "https://www.youtube.com/embed/Z4XQWOVs2_g?rel=0&autoplay=1";
+                    model.Info = "";
                     return model;
 
                 case GameType.Subtraction:
-                    model.Info = "https://www.youtube.com/embed/WQaT1rGs8ao?rel=0&autoplay=1";
+                    model.Info = "";
                     return model;
 
 
